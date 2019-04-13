c2e1ef4d47afd7cddd59b3a8713ff0f55f10a829
Axel S <saxelsyeguillaume@gmail.com>
2019-04-13 10:35:42 +0200

    Script +  & tests
    script C/U/D Items, tasks & colloc
    test for colloc

--- src/ITI.Roomies.DB/Scripts/000002.CreateTable-tColloc.sql
+++ src/ITI.Roomies.DB/Scripts/000002.CreateTable-tColloc.sql
@@ -2,11 +2,11 @@ create table rm.tColloc
 (
 	CollocId int identity(0,1),
 	CollocName nvarchar(32) not null,
-	CreationDate datetime2 not null,
-	CollocPic nvarchar(45),
+	CreationDate datetime2 default getdate(),
+	CollocPic nvarchar(64),
 
 	constraint PK_rm_tColloc primary key( CollocId ),
 	constraint CK_rm_tColloc_CollocName check( CollocName <> N'') 
 );
-insert into rm.tColloc( CollocName, CreationDate) 
-values                ( left(convert(nvarchar(36), newid()), 32),'19910101');	
+insert into rm.tColloc( CollocName ) 
+values                ( left(convert(nvarchar(36), newid()), 32));	
