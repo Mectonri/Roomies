1a9d8bd5dc71bf9702d8045bb0bce87203cc1a29
mectonri <thibault.leclerc013@gmail.com>
2019-04-16 15:11:42 +0200

    Home page

--- src/ITI.Roomies.Spa/src/components/Login.vue
+++ src/ITI.Roomies.Spa/src/components/Login.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="container p-3">
+
+    <div class="main">
+        
         <div class="text-center">
             <h1 class="my-4">Bienvenue sur Roomies</h1>
-
+        </div>
+        <div>
             <button type="button" @click="login('Google')" class="btn btn-lg btn-block btn-primary">
                 <i class="fa fa-google" aria-hidden="true"></i> Se connecter via Google</button>
             
@@ -14,6 +17,13 @@
 <script>
 import AuthService from '../services/AuthService'
 import Vue from 'vue'
+/*import onepagescroll from '../styles/onepagescroll'
+
+onepagescroll(".main", {
+     sectionContainer: "section",
+     loop: true,
+     responsiveFallback: false
+   });*/
 
 export default {
     data() {
