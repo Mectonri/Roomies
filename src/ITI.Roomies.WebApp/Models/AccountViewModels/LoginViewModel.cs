14d37beb7dba868848f1a4153dbfda5f62f194e5
mectonri <thibault.leclerc013@gmail.com>
2019-04-18 09:41:39 +0200

    Fix Login
    fix where the login was asking for informations it should not have asked

--- src/ITI.Roomies.WebApp/Models/AccountViewModels/LoginViewModel.cs
+++ src/ITI.Roomies.WebApp/Models/AccountViewModels/LoginViewModel.cs
@@ -11,17 +11,5 @@ namespace ITI.Roomies.WebApp.Models.AccountViewModels
         [Required]
         [DataType( DataType.Password )]
         public string Password { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
-
-        [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string BirthDate { get; set; }
-
-        [Required]
-        public string PhoneNumber { get; set; }
     }
 }
