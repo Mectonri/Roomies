9f6af837ce868c455b9ad9fc5d29c3ab73d409e9
Audran COLLIGNON <audran.collignon@gmail.com>
2019-05-09 11:01:42 +0200

    Fix currColloc lors d'une déco

--- src/ITI.Roomies.Spa/src/components/Logout.vue
+++ src/ITI.Roomies.Spa/src/components/Logout.vue
@@ -1,39 +1,41 @@
 <template>
-    <div class="p-5 text-center">
-        <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
-        <br />
-        <h1>Déconnexion en cours...</h1>
+  <div class="p-5 text-center">
+    <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+    <br>
+    <h1>Déconnexion en cours...</h1>
 
-        <iframe :src="logoutEndpoint" frameborder="0" width="0" height="0"></iframe>
-    </div>
+    <iframe :src="logoutEndpoint" frameborder="0" width="0" height="0"></iframe>
+  </div>
 </template>
 
 <script>
-    import AuthService from '../services/AuthService'
+import AuthService from "../services/AuthService";
 
-    export default {
-        mounted() {
-            AuthService.registerSignedOutCallback(() => this.onSignedOut());
-        },
+export default {
+  mounted() {
+    AuthService.registerSignedOutCallback(() => this.onSignedOut());
+  },
 
-        beforeDestroy() {
-            AuthService.removeSignedOutCallback(() => this.onSignedOut());
-        },
+  beforeDestroy() {
+    AuthService.removeSignedOutCallback(() => this.onSignedOut());
+  },
 
-        computed: {
-            logoutEndpoint: () => AuthService.logoutEndpoint
-        },
+  computed: {
+    logoutEndpoint: () => AuthService.logoutEndpoint
+  },
 
-        methods: {
-            onSignedOut() {
-                // Cache le menu de navigation avant la déconnexion
-                document.getElementById("navMenu").style.display = "none";
-                this.$router.replace('/');
-            }
-        }
+  methods: {
+    onSignedOut() {
+      // Cache le menu de navigation avant la déconnexion
+      document.getElementById("navMenu").style.display = "none";
+      // Reset les variable globales
+      this.$currColloc.setCollocId(0);
+      this.$currColloc.setCollocName("");
+      this.$router.replace("/");
     }
+  }
+};
 </script>
 
 <style lang="scss">
-
 </style>
