ecc0aa2684bf21df8df0026987ea0ecdb7b2c711
Audran COLLIGNON <audran.collignon@gmail.com>
2019-05-15 15:29:04 +0200

    Fix CSS

--- src/ITI.Roomies.Spa/src/components/Collocation.vue
+++ src/ITI.Roomies.Spa/src/components/Collocation.vue
@@ -1,73 +1,77 @@
 <template>
-    <div class="container">
-      <header>
-        <button class="btn btn-dark" @click="changeCreate()">création</button>
-        <button class="btn btn-dark" @click="changeInvite()">inviter</button>
-        <button class="btn btn-dark" @click="changeJoin()">joindre</button>
-      </header>
-      <div v-if="show1">
-        <form @submit="onSubmit($event)">
-          <div class="alert alert-danger" v-if="errors.length > 0">
-            <b>Les champs suivants semblent invalides :</b>
+  <div class="container">
+    <header>
+      <button class="btn btn-dark" @click="changeCreate()">création</button>
+      <button class="btn btn-dark" @click="changeInvite()">inviter</button>
+      <button class="btn btn-dark" @click="changeJoin()">joindre</button>
+    </header>
+    <div v-if="show1">
+      <form @submit="onSubmit($event)">
+        <div class="alert alert-danger" v-if="errors.length > 0">
+          <b>Les champs suivants semblent invalides :</b>
 
-            <ul>
-              <li v-for="e of errors">{{e}}</li>
-            </ul>
-          </div>
+          <ul>
+            <li v-for="e of errors">{{e}}</li>
+          </ul>
+        </div>
 
-          <div class="form-group">
-            <label class="required">Nom de collocation</label>
-            <input class="form-control" type="text" v-model="item.CollocName" required/>
-          </div>
+        <div class="form-group">
+          <label class="required">Nom de collocation</label>
+          <input class="form-control" type="text" v-model="item.CollocName" required>
+        </div>
 
-          <br>
-          <button class="btn btn-dark" native-type="submit" v-if="this.collocName==''">Sauvegarder</button>
-          <p v-if="this.collocName!='' ">Vous avez déjà une collocation.</p>
-        </form>
-      </div>
-      <div v-if="show3">
-        <form @submit="onSubmitJoin($event)">
-          <div class="form-group">
-            <label class="required">Clé :</label>
-            <input class="form-control" type="text" v-model="item.InviteKey" required/>
-          </div>
-
-          <br>
-          <button class="btn btn-dark" native-type="submit" v-if="this.collocName==''">Rejoindre</button>
-          <p v-if="this.collocName!=''">Vous avez déjà une collocation.</p>
-          <p v-if="this.checkjoin==0">le code que vous avez rentré n'est pas valide.</p>
-        </form>
-      </div>
-
-      <div v-if="show2">
-        <form @submit="onSubmitInvite($event)">
-          <div class="alert alert-danger" v-if="errors.length > 0">
-            <b>Les champs suivants semblent invalides :</b>
-
-            <ul>
-              <li v-for="e of errors">{{e}}</li>
-            </ul>
-          </div>
-
-          <div class="form-group">
-            <label class="required">Mail</label>
-            <input class="form-control" type="text" v-model="item.mail" required/>
-          </div>
-
-          <button class="form-control" native-type="submit" v-if="this.collocName!=''">Envoyer</button>
-          <p
-            v-if="this.collocName==''"
-          >Veuillez d'abords créer une collocation avant de chercher à inviter des personnes.</p>
-          <p v-if="this.checkInvite==0">Le mail que vous avez rentré ne correspond à aucun roomie.</p>
-          <p v-if="this.checkInvite==1">L'invitation a été envoyé</p>
-        </form>
-      </div>
-
-      <div v-if="this.collocName!='' && show4">
         <br>
-        <button class="btn btn-dark" @click="onSubmitQuit($event)" native-type="submit">Quitter la collocation</button>
-      </div>
+        <button class="btn btn-dark" native-type="submit" v-if="this.collocName==''">Sauvegarder</button>
+        <p v-if="this.collocName!='' ">Vous avez déjà une collocation.</p>
+      </form>
     </div>
+    <div v-if="show3">
+      <form @submit="onSubmitJoin($event)">
+        <div class="form-group">
+          <label class="required">Clé :</label>
+          <input class="form-control" type="text" v-model="item.InviteKey" required>
+        </div>
+
+        <br>
+        <button class="btn btn-dark" native-type="submit" v-if="this.collocName==''">Rejoindre</button>
+        <p v-if="this.collocName!=''">Vous avez déjà une collocation.</p>
+        <p v-if="this.checkjoin==0">le code que vous avez rentré n'est pas valide.</p>
+      </form>
+    </div>
+
+    <div v-if="show2">
+      <form @submit="onSubmitInvite($event)">
+        <div class="alert alert-danger" v-if="errors.length > 0">
+          <b>Les champs suivants semblent invalides :</b>
+
+          <ul>
+            <li v-for="e of errors">{{e}}</li>
+          </ul>
+        </div>
+
+        <div class="form-group">
+          <label class="required">Mail</label>
+          <input class="form-control" type="text" v-model="item.mail" required>
+        </div>
+
+        <button class="form-control" native-type="submit" v-if="this.collocName!=''">Envoyer</button>
+        <p
+          v-if="this.collocName==''"
+        >Veuillez d'abords créer une collocation avant de chercher à inviter des personnes.</p>
+        <p v-if="this.checkInvite==0">Le mail que vous avez rentré ne correspond à aucun roomie.</p>
+        <p v-if="this.checkInvite==1">L'invitation a été envoyé</p>
+      </form>
+    </div>
+
+    <div v-if="this.collocName!='' && show4">
+      <br>
+      <button
+        class="btn btn-dark"
+        @click="onSubmitQuit($event)"
+        native-type="submit"
+      >Quitter la collocation</button>
+    </div>
+  </div>
 </template>
 
 
@@ -193,3 +197,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.form-control {
+  max-width: 200em;
+}
+</style>
