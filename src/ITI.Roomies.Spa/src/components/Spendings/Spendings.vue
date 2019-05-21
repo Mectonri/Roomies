088f05733b0c87159c4bbf323691ec430318e668
Axel S <saxelsyeguillaume@gmail.com>
2019-05-21 14:42:01 +0200

    Chartjs 1st implemantation

---
+++ src/ITI.Roomies.Spa/src/components/Spendings/Spendings.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="app">
+      <canvas id="planet-chart"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+import planetChartData from "../Spendings/Chart-Data.js";
+
+export default {
+  data() {
+    return {
+      planetChartData: planetChartData
+    };
+  },
+
+  async mounted() {
+    debugger;
+    this.createChart("planet-chart", this.planetChartData);
+  },
+
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      });
+    }
+  }
+};
+</script>
+
