df174823bea387b411883cb9d0e7d9ee9c255127
Axel S <saxelsyeguillaume@gmail.com>
2019-05-21 13:52:37 +0200

    RItem WiP

---
+++ src/ITI.Roomies.DAL/RItemData.cs
@@ -0,0 +1,13 @@
+namespace ITI.Roomies.DAL
+{
+    public class RItemData
+    {
+        public int ItemId { get; set; }
+
+        public int ItemPrice { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int CourseId { get; set; }
+    }
+}
