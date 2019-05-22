6bda0c468fdfeca34a26738ec6fbf86f7c62d46a
Axel S <saxelsyeguillaume@gmail.com>
2019-05-22 14:26:06 +0200

    Transaction

---
+++ src/ITI.Roomies.DAL/TransacBudgetData.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ITI.Roomies.DAL
+{
+    public class TransacBudgetData
+    {
+        public int TBudgetId { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int BudgetId { get; set; }
+
+        public int RoomieId { get; set; }
+    }
+
+}
