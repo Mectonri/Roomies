48bfb03fe727ed46e11796d4b002a28e454ef909
Axel S <saxelsyeguillaume@gmail.com>
2019-05-26 10:40:49 +0200

    back end Colloc picture upload

---
+++ Nouveau dossier/PicturesData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pick_n_Trip.DAL.tPictures
+{
+    public class PicturesData
+    {
+        int IdPictures { get; set; }
+        int IdBelong { get; set; }
+        string Link { get; set; }
+    }
+}
